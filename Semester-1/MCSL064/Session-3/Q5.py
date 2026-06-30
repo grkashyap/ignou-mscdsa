6b23261484fb7b059e4389548f83bfa06b9db3d8
rahul <grkashyap@gmail.com>
2026-06-30 22:38:36 +0530

    Updated with correct implementation

--- Semester-1/MCSL064/Session-3/Q5.py
+++ Semester-1/MCSL064/Session-3/Q5.py
@@ -1,22 +1,23 @@
-def count_frequency(input_str: str, freq_dict: dict, index: int = 0) -> dict:
+def count_frequency(words: list, freq_dict: dict, index: int = 0) -> dict:
 
-    if index == len(input_str):
+    if index == len(words):
         return freq_dict
 
-    word = input_str[index]
+    word = words[index]
 
     if word == ' ':
-        return count_frequency(input_str, freq_dict, index+1)
+        return count_frequency(words, freq_dict, index + 1)
 
     if word in freq_dict:
         freq_dict[word] += 1
     else:
         freq_dict[word] = 1
 
-    return count_frequency(input_str, freq_dict, index+1)
+    return count_frequency(words, freq_dict, index + 1)
 
 
 if __name__=='__main__':
     input_str = input('Enter a string: ')
-    freq_dict = count_frequency(input_str, {})
+    words = input_str.split()
+    freq_dict = count_frequency(words, {})
     print(freq_dict)
