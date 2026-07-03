8148df520218108cf574c714ad18d63672380cfc
rahul <grkashyap@gmail.com>
2026-07-03 21:29:31 +0530

    Updated with correct implementation

--- Semester-1/MCSL064/Session-5/Q10.py
+++ Semester-1/MCSL064/Session-5/Q10.py
@@ -4,21 +4,13 @@ def generate_subsets(input_set: list, subsets: list[set], index: int = 0) -> lis
         return [set()]
 
     value = input_set[index]
-
     subsets.append(set(value))
-
     remaining_subsets = generate_subsets(input_set, subsets, index + 1)
-
     subsets = [subset.union({value}) for subset in remaining_subsets]
-
     return subsets + remaining_subsets
 
 if __name__ == '__main__':
-    str_lst = input('Enter values of set separated by space in a single line: ')
-    str_lst_arr = str_lst.split(' ')
-    input_set = []
-    for item in str_lst_arr:
-        input_set.append(item)
-
+    str_lst = input('Enter values of set separated by comma in a single line: ')
+    input_set = str_lst.split(',')
     subset_lst = generate_subsets(input_set, [])
     print(subset_lst)
